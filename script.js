f49d02aa3b3c356645e053a4d0499400408a19a6
Rishabh Sharma <sharmarishabh417@gmail.com>
2021-06-08 13:38:15 +0530

    initial setup

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let allFilterTags = document.querySelectorAll(".ticket-filters div");
+let arrangeBtn = document.querySelector(".arrange-btn");
+arrangeBtn.addEventListener("click", arrangeDB);
 let ticketContainer = document.querySelector(".tickets-container");
 let openModalBtn = document.querySelector(".open-modal");
 let closeModalBtn = document.querySelector(".close-modal");
@@ -338,5 +340,29 @@ function loadSelectedTickets(ticketFilter) {
         }
 
         //console.log(filteredTickets);
+    }
+}
+
+function arrangeDB() {
+    if (localStorage.getItem("allTickets")) {
+        let allTickets = JSON.parse(localStorage.getItem("allTickets"));
+        let newAllTickets = [];
+
+        insertCurrColorTicketsInNewDb(allTickets, "black", newAllTickets);
+        insertCurrColorTicketsInNewDb(allTickets, "red", newAllTickets);
+        insertCurrColorTicketsInNewDb(allTickets, "orange", newAllTickets);
+        insertCurrColorTicketsInNewDb(allTickets, "green", newAllTickets);
+
+        localStorage.setItem("allTickets", JSON.stringify(newAllTickets));
+        loadTickets();
+    }
+}
+
+function insertCurrColorTicketsInNewDb(allTickets, currColorArrange, newAllTickets) {
+    for (let i = 0; i < allTickets.length; i++) {
+        let ticketObject = allTickets[i];
+        if (ticketObject.ticketFilter == currColorArrange) {
+            newAllTickets.push(ticketObject);
+        }
     }
 }
